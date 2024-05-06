105d12c1683b14f5f87b5bc970b28a388b3c8e25
paxblueribbon <veganbikepunk@gmail.com>
2024-05-06 16:49:29 -0700

    added full parameter to user get requests

--- routes/user/user.helper.js
+++ routes/user/user.helper.js
@@ -1,20 +1,61 @@
 const User = require('../../sql-database').User
+const Chat = require('../../sql-database').Chat
 
 const createUser = async ({ name, password, role, email }) => { 
   return await User.create({ name, password, role, email });
 };
-const getAllUsers = async () => {
-  return await User.findAll();
+const getAllUsers = async (full) => {
+  if (full) {
+    return await User.findAll({
+      include: [
+        {
+          model: Chat,
+          as: "chats"
+        }
+      ]
+    })
+  }
+  else {
+  return await User.findAll({  
+  });
+}
 };
-const getUser = async obj => {
-  return await User.findOne({
-  where: obj,
-});
+const getUser = async (obj, full) => {
+  if (full) {
+    return await User.findOne({
+      where: obj,
+      include: [
+        {
+          model: Chat,
+          as: 'chats'
+        }
+      ]
+    })
+  }
+  else {
+    return await User.findOne({
+      where: obj,
+    });
+  }
 };
-const getUserByID = async function(id) {
-  return await User.findOne({
-  where: {id: id},
-});
+const getUserByID = async function(id, full) {
+  if (full) {
+    return await User.findOne({
+      where: {id: id},
+      include: [
+        {
+          model: Chat,
+          as: 'chats'
+        }
+      ]
+    })
+  }
+  else {
+    return await User.findOne({
+      where: {id: id},
+    });
+  }
+
 };
 
 module.exports = { createUser, getAllUsers, getUser, getUserByID }

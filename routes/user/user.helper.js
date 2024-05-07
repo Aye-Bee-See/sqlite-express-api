612dd20bdcf02dc8647f8ab87776b950cf11b293
mcoatney <mcoatney@tutanota.com>
2024-05-07 01:11:11 -0700

    Rebase merge

--- routes/user/user.helper.js
+++ routes/user/user.helper.js
@@ -1,27 +1,26 @@
-const { name } = require('./user.model');
+const {name} = require('./user.model');
 
 const User = require('../../sql-database').User
 const Chat = require('../../sql-database').Chat
 
-const createUser = async ({ name, password, role, email }) => { 
-  return await User.create({ name, password, role, email });
+const createUser = async ({ name, password, role, email }) => {
+    return await User.create({name, password, role, email});
 };
 
 const getAllUsers = async (full) => {
-  if (full) {
-    return await User.findAll({
-      include: [
-        {
-          model: Chat,
-          as: "chats"
-        }
-      ]
-    })
-  }
-  else {
-  return await User.findAll({  
-  });
-}
+    if (full) {
+        return await User.findAll({
+            include: [
+                {
+                    model: Chat,
+                    as: "chats"
+                }
+            ]
+        })
+    } else {
+        return await User.findAll({
+        });
+    }
 };
 
 /**
@@ -29,7 +28,7 @@ const getAllUsers = async (full) => {
  *  
  *  @param {array} userArray  - Array of user params
  */
-const createBulkUsers=async (userArray)=>{
+const createBulkUsers = async (userArray) => {
     return await User.bulkCreate(userArray);
 };
 
@@ -37,103 +36,98 @@ const createBulkUsers=async (userArray)=>{
  * Get raw user count
  * @returns {int} 
  */
-const countUsers= async()=>{
+const countUsers = async() => {
     const {count} = await User.findAndCountAll();
     return count;
 };
 
 const getUser = async (obj, full) => {
-  if (full) {
-    return await User.findOne({
-      where: obj,
-      include: [
-        {
-          model: Chat,
-          as: 'chats'
-        }
-      ]
-    })
-  }
-  else {
-    return await User.findOne({
-      where: obj,
-    });
-  }
+    if (full) {
+        return await User.findOne({
+            where: obj,
+            include: [
+                {
+                    model: Chat,
+                    as: 'chats'
+                }
+            ]
+        })
+    } else {
+        return await User.findOne({
+            where: obj,
+        });
+    }
 };
-const getUserByID = async function(id, full) {
-  if (full) {
-    return await User.findOne({
-      where: {id: id},
-      include: [
-        {
-          model: Chat,
-          as: 'chats'
-        }
-      ]
-    })
-  }
-  else {
-    return await User.findOne({
-      where: {id: id},
-    });
-  }
+const getUserByID = async function (id, full) {
+    if (full) {
+        return await User.findOne({
+            where: {id: id},
+            include: [
+                {
+                    model: Chat,
+                    as: 'chats'
+                }
+            ]
+        })
+    } else {
+        return await User.findOne({
+            where: {id: id},
+        });
+    }
 };
 
-const getUserByEmail = async function(email, full) {
-  if (full) {
-    return await User.findOne({
-      where: {email: email},
-      include: [
-        {
-          model: Chat,
-          as: 'chats'
-        }
-      ]
-    })
-  }
-  else {
-    return await User.findOne({
-      where: {email: email},
-    });
-  }
+const getUserByEmail = async function (email, full) {
+    if (full) {
+        return await User.findOne({
+            where: {email: email},
+            include: [
+                {
+                    model: Chat,
+                    as: 'chats'
+                }
+            ]
+        })
+    } else {
+        return await User.findOne({
+            where: {email: email},
+        });
+    }
 };
 
 const getUserByName = async (name, full) => {
-  if (full) {
-    return await User.findOne({
-      where: {name: name},
-      include: [
-        {
-          model: Chat,
-          as: 'chats'
-        }
-      ]
-    })
-  }
-  else {
-    return await User.findOne({
-      where: {name: name},
-    });
-  }
+    if (full) {
+        return await User.findOne({
+            where: {name: name},
+            include: [
+                {
+                    model: Chat,
+                    as: 'chats'
+                }
+            ]
+        })
+    } else {
+        return await User.findOne({
+            where: {name: name},
+        });
+    }
 }
 
 const getUserByNameOrEmail = async (name, email, full) => {
-  if (full) {
-    return await User.findOne({
-      where: {name: name, email: email},
-      include: [
-        {
-          model: Chat,
-          as: 'chats'
-        }
-      ]
-    })
-  }
-  else {
-    return await User.findOne({
-      where: {name: name, email: email},
-    });
-  }
+    if (full) {
+        return await User.findOne({
+            where: {name: name, email: email},
+            include: [
+                {
+                    model: Chat,
+                    as: 'chats'
+                }
+            ]
+        })
+    } else {
+        return await User.findOne({
+            where: {name: name, email: email},
+        });
+    }
 }
 
-module.exports = { createUser, createBulkUsers, countUsers, getAllUsers, getUser, getUserByID, getUserByEmail, getUserByName, getUserByNameOrEmail }
+module.exports = {createUser, createBulkUsers, countUsers, getAllUsers, getUser, getUserByID, getUserByEmail, getUserByName, getUserByNameOrEmail}

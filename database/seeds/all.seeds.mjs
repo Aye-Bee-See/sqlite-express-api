671577df9fe61b8f1225852c8dfd67920ec3fb14
paxblueribbon <veganbikepunk@gmail.com>
2024-09-03 19:28:03 -0700

    Added json files for seed, run seed functions in all.seeds.mjs

--- database/seeds/all.seeds.mjs
+++ database/seeds/all.seeds.mjs
@@ -1,9 +1,24 @@
 import {createUserSeed} from  '#db/seeds/user.seed.mjs';
+import {createPrisonSeed} from  '#db/seeds/prison.seed.mjs';
+import {createPrisonerSeed} from  '#db/seeds/prisoner.seed.mjs';
+import {createRuleSeed} from  '#db/seeds/rule.seed.mjs';
+import {createChatSeed} from  '#db/seeds/chat.seed.mjs';
+import {createMessageSeed} from  '#db/seeds/message.seed.mjs';
 
+/*
+Order of seeding:
+User
+Prison
+Prisoner (Requires Prison)
+Rules (Requires Prison)
+Chat (Requires Prisoner and User)
+Message (Requires Chat)
+*/
 
 export async function createSeeds() {
     return Promise.all([
         createUserSeed()
+
     ]).then(() => {
         console.log('Seeds created');
     });

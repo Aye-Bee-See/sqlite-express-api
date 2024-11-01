8da98cbc88f90df702d005a85ed7205e2b35a5aa
aowe@github.com <aowe@tutanota.com>
2024-10-31 21:02:21 -0700

    Create es6 user.js currently empty

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,61 +1,6 @@
-import express,{Router as router} from 'express';
-import {default as bodyParser} from 'body-parser';
-import {default as passport} from 'passport';
-import {userEnd} from '#routes/constants.js';
-import {default as userCrtlr} from "#rtControllers/user.controller.mjs";
-import authService from "#rtServices/auth.services.mjs";
-
-class UserRoutes {
-
-    static Router;
-    static #Controller;
-
-    /************************************************************
-     *                                                          *
-     *                  STATIC INIT BLOCK                       *
-     *                                                          *
-     *   Initialize all necessary parts of the class            *
-     ************************************************************/
-    static {
-       const app=express();
-       app.use(bodyParser.json());
-       app.use(bodyParser.urlencoded({extended: true}));
-       app.use(passport.initialize());
-       
-       const UserJWTStrat = authService.authorize;
-       const LoginStrat = authService.login;
-       passport.use('UsrJStrat', UserJWTStrat);
-       passport.use('LStrat', LoginStrat);
-       
-       
-       this.#Controller= new userCrtlr;
-       this.Router = express.Router();
-       
-       this.#router();
-   }
-    /***
-     * 
-     *   Handle router params
-     * 
-     ***/
-    static #router(){
-        // Create
-        this.Router.post(userEnd.post.create, this.#Controller.create);
-        
-        // Login
-        this.Router.post(userEnd.post.login, passport.authenticate('LStrat',{ session: false }), this.#Controller.login);
-        
-        // Read
-        this.Router.get(userEnd.get.many, passport.authenticate('UsrJStrat', { session: false }), this.#Controller.getMany);
-        this.Router.get(userEnd.get.one, passport.authenticate('UsrJStrat', { session: false }), this.#Controller.getOne);
-        
-        // Update
-        this.Router.put(userEnd.put.update, passport.authenticate('UsrJStrat', {session: false}), this.#Controller.update);
-        
-        // Delete
-        this.Router.delete(userEnd.delete.remove, passport.authenticate('UsrJStrat', {session: false}), this.#Controller.remove);
-    }
-}
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
 
 
-export default UserRoutes;

f30bfa1f592cf92ded67f6eda0654ff11dd0c8b4
aowe@github.com <aowe@tutanota.com>
2024-12-03 23:57:34 -0800

    Protect routes, add cors

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import PrisonerRoutes from '#routes/prisoner/prisoner.js';
 import RuleRoutes from '#routes/rule/rule.mjs';
 import MessageRoutes from '#routes/message/message.js';
 import ChatRoutes from '#routes/chat/chat.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -31,29 +32,6 @@ app.use(passport.initialize());
 app.listen(sysPort, function () {
     console.log('Express is running on port: ' + sysPort);
 });
-
-
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, authorization');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-
 
 app.use('/auth', authRouter.Router);
 app.use('/prison', prisonRoutes.Router);

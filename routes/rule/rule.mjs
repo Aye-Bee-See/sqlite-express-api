adf0cb03605e58623c7dea5f933ee3e44e6cb941
aowe@github.com <aowe@tutanota.com>
2025-01-18 00:49:15 -0800

    Closed branch but I forgot to push this for rules before merging to main so I'm using it as my hotfix since it belonged in that merge

--- routes/rule/rule.mjs
+++ routes/rule/rule.mjs
@@ -40,21 +40,21 @@ class RuleRoutes {
 
 // Create
 
-        this.Router.post(ruleEnd.post.create, passport.authenticate('UsrJStrat', { session: false }), this.#Controller.create);
+        this.Router.post(ruleEnd.post.create, passport.authenticate('UsrJStrat', { session: false, failWithError: true }), this.#Controller.create);
 
 // Read
 
-        this.Router.get(ruleEnd.get.many, passport.authenticate('UsrJStrat', { session: false }), this.#Controller.getMany);
+        this.Router.get(ruleEnd.get.many, passport.authenticate('UsrJStrat', { session: false, failWithError: true }), this.#Controller.getMany);
 
-        this.Router.get(ruleEnd.get.one, passport.authenticate('UsrJStrat', { session: false }), this.#Controller.getOne);
+        this.Router.get(ruleEnd.get.one, passport.authenticate('UsrJStrat', { session: false, failWithError: true }), this.#Controller.getOne);
 
 // Update
 
-        this.Router.put(ruleEnd.put.update, passport.authenticate('UsrJStrat', { session: false }), this.#Controller.update);
+        this.Router.put(ruleEnd.put.update, passport.authenticate('UsrJStrat', { session: false, failWithError: true }), this.#Controller.update);
 
 // Delete
 
-        this.Router.delete(ruleEnd.delete.remove, passport.authenticate('UsrJStrat', { session: false }), this.#Controller.remove);
+        this.Router.delete(ruleEnd.delete.remove, passport.authenticate('UsrJStrat', { session: false, failWithError: true }), this.#Controller.remove);
     }
 }
 
